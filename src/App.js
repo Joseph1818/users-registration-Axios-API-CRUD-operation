aa61b9f0566c4174891d387aa09f57ab9cb233e4
Joseph1818 <ujee@UMBAS-MacBook-Air.local>
2023-11-15 15:29:14 +0200

    Read page added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import "./App.css";
-import Create from "./components/create";
+import Create from "./component/create";
+import Read from "./component/read";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  BrowserRouter,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="main">
-      <h2 className="main-header">Users registration Form</h2>
-      <Create />
-    </div>
+    <Router>
+      <div className="main">
+        <h2 className="main-header">Users Registration form</h2>
+        <Routes>
+          <Route exact path="/create" element={<Create />} />
+          <Route exact path="/read" element={<Read />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/component/create.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { Button, Checkbox, Form } from "semantic-ui-react";
+
+export default function Create() {
+  //Created useState for first-name and last name to get data.
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [checkbox, setCheckbox] = useState(false);
+  // function post Data to post Data into the API
+  const postData = () => {
+    axios.post("https://6554786963cafc694fe68229.mockapi.io/fakeData", {
+      firstName,
+      lastName,
+      checkbox,
+    });
+
+    // console.log(firstName);
+    // console.log(lastName);
+    // console.log(checkbox);
+  };
+  return (
+    <div>
+      <Form className="create-form">
+        <Form.Field>
+          <label>First Name</label>
+          {/* Setting the components into a functional components */}
+          <input
+            placeholder="First Name"
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Last Name</label>
+          <input
+            placeholder="Last Name"
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox
+            label="I agree to the Terms and Conditions"
+            onChange={(e) => setCheckbox(!checkbox)}
+          />
+        </Form.Field>
+        <Button type="submit" onClick={postData}>
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+}
